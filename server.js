03b269c2cfe2aba034bff0b632f6ba810b5fe8f1
chamuditha01 <heshanchamuditha05@gmail.com>
2025-06-03 10:45:47 +0530

    contract updated

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 const RPC_URL = "https://testnet.hashio.io/api"; // Replace if unstable
 const PRIVATE_KEY =
   "7b5c03deb9f5056f07d3f4e934c1d051832fc62a088ba361d02af083eb07b5f7";
-const CONTRACT_ADDRESS = "0x79474DEFc030bbE60471AC4070d810FD6E688b25";
+const CONTRACT_ADDRESS = "0x5310E12D33A1Cdcf8CE105EDC2E9A66B9D61eed0";
 const MAX_RETRIES = 3;
 
 const CONTRACT_ABI = [
